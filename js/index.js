7910bd6b26633a0e20ab4cffbd7787c1f2fbfced
Ndhossain <hhhssnahiid@gmail.com>
2023-01-03 19:12:18 +0600

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-$(document).ready(function () {
-    $('.counter-number').counterUp({
-        delay: 10,
-        time: 1800
-    })
-})
+$(function () {
+  $(".counter-number").counterUp({
+    delay: 10,
+    time: 1800,
+  });
+});
+
+$(function () {
+  $(".date").text(function () {
+    const date = new Date();
+    let year = date.getFullYear();
+    return year;
+  });
+});
